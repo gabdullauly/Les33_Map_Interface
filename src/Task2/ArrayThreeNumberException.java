0ecd758caac1a4e35774947e5b6e3c84287f1350
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2022-03-30 21:57:12 +0600

    йцуйцу

--- src/Task2/ArrayThreeNumberException.java
+++ src/Task2/ArrayThreeNumberException.java
@@ -1,16 +1,20 @@
 package Task2;
 
 public class ArrayThreeNumberException extends Exception {
-    private int index = 7;
+    private int index;
+    private String message;
     public ArrayThreeNumberException(){}
-    public ArrayThreeNumberException(int index){
+    public ArrayThreeNumberException(int index, String message){
         this.index = index;
+        this.message = message;
     }
     public int getIndex(){
         return index;
     }
+    public String getMessage(){
+        return message;
+    }
     public void setIndex(int index){
         this.index = index;
     }
-
 }
